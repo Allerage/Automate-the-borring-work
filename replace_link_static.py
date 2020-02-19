57edc21b6fdc22d2668a8378a62ba60e4ecaa439
Allerage <52875331+Allerage@users.noreply.github.com>
2020-02-19 17:42:25 +0530

    club your non django to django template

--- replace_link_static.py
+++ replace_link_static.py
@@ -3,28 +3,61 @@ from bs4 import BeautifulSoup
 from html.parser import HTMLParser
 
 import sys
+import os
 
 
-ha = open(r"F:\automation scripts\portfoilio\Template\elements.html",
-          encoding='utf-8')
-soup = BeautifulSoup(ha, 'html.parser')
-n = soup.find_all("img")
-cs_link = soup.find_all("link")
-sc_link = soup.find_all('script')
-print(cs_link)
-
-ha.close()
 static_path = []
 
 
-def replace_s(sp):
-    with open(r"F:\automation scripts\portfoilio\Template\elements.html", encoding="utf-8") as nw:
-        nst = open("newindext.html", "w+", encoding="utf-8")
+def walk_folder():
+
+    for root, d, f in os.walk(r"F:\automation scripts\portfoilio\Template"):
+        print(root)
+        print("--")
+        print(d)
+        jh = f
+    print(jh)
+    try:
+        os.mkdir(os.getcwd() + '\\tempdjango')
+    except:
+        pass
+
+    for df in jh:
+
+        ha = open(r"F:\automation scripts\portfoilio\Template\{}".format(df) ,
+                  encoding='utf-8')
+        soup = BeautifulSoup(ha, 'html.parser')
+        n = soup.find_all("img")
+        cs_link = soup.find_all("link")
+        sc_link = soup.find_all('script')
+        print(cs_link)
+
+        ha.close()
+        for j in cs_link:
+            static_path.append(j['href'])
+            print(j['href'])
+        for i in n:
+            static_path.append(i['src'])
+    # print(static_path)
+        print(static_path)
+        print(sc_link)
+        for s in sc_link:
+
+            try:
+                static_path.append(s['src'])
+            except:
+                pass
+        replace_s(static_path,df)
+
+
+def replace_s(sp,file_name):
+    with open(r"F:\automation scripts\portfoilio\Template\{}".format(file_name), encoding="utf-8") as nw:
+        nst = open(os.path.join(os.getcwd() + '\\tempdjango',file_name),
+                   "w+", encoding="utf-8")
         n = nw.readlines()
         for i in n:
             for j in sp:
                 if (i.find(j) == -1):
-
 
                     nst.write(i)
 
@@ -40,9 +73,6 @@ def replace_s(sp):
 
         print("finish")
 
-
-
-def replace_a_img():
     pass
 
 
@@ -63,22 +93,7 @@ def basic_setting():
     print(n)
 
 
-for j in cs_link:
-    static_path.append(j['href'])
-    print(j['href'])
-for i in n:
-    static_path.append(i['src'])
-    # print(static_path)
-print(static_path)
-print(sc_link)
-for s in sc_link:
-    try:
-        static_path.append(s['src'])
-    except:
-        pass
-
-
-
 # replace_s(static_path)
 # j=static_path[0]
-replace_s(static_path)
+# replace_s(static_path)
+walk_folder()
